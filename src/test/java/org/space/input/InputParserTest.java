ebd92249cf8bb56517d2c108bd6f3cf30e1e4cbe
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-16 13:31:51 +0100

    test PlateauSize Invalid Arguments throw IllegalArgumentException

--- src/test/java/org/space/input/InputParserTest.java
+++ src/test/java/org/space/input/InputParserTest.java
@@ -1,6 +1,8 @@
 package org.space.input;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.space.PlateauSize;
 
 class InputParserTest {
@@ -15,6 +17,22 @@ class InputParserTest {
         Assertions.assertEquals(5,plateauSize.getWidth());
         Assertions.assertEquals(7, plateauSize.getHeight());
 
+    }
+
+    @Test
+    @DisplayName("PlateauSize given invalid format should throw IllegalArgumentException")
+    void testParsePlateauSize_InvalidFormat(){
+        String input = "5";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {InputParser.parsePlateauSize(input);});
+    }
+
+    @Test
+    @DisplayName("PlateauSize given invalid format should throw IllegalArgumentException")
+    void testParsePlateauSize_NonNumericInput(){
+        String input = "5 a";
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {InputParser.parsePlateauSize(input);});
     }
 
     @org.junit.jupiter.api.Test
