f694ffedfdfdc5cb0e659ca0fb18e6a088f78043
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-16 17:39:15 +0100

    Implement class Rover rotate method. Updated tests

--- src/main/java/org/space/logic/Rover.java
+++ src/main/java/org/space/logic/Rover.java
@@ -12,6 +12,51 @@ Position position;
     }
 
     public CompassDirection rotate(Instruction instruction){
-        return null;
+        var currentlyFacing = position.getFacing();
+        CompassDirection nextDirection;
+        if(instruction == Instruction.M)
+            return currentlyFacing;
+        else if (instruction == Instruction.L) {
+            nextDirection = rotateLeft(currentlyFacing);
+        }else {
+            nextDirection = rotateRight(currentlyFacing);
+        }
+        return nextDirection;
+    }
+
+    private CompassDirection rotateLeft(CompassDirection currentlyFacing){
+        switch (currentlyFacing) {
+            case N:
+                currentlyFacing = CompassDirection.W;
+                break;
+            case E:
+                currentlyFacing = CompassDirection.N;
+                break;
+            case S:
+                currentlyFacing = CompassDirection.E;
+                break;
+            case W:
+                currentlyFacing = CompassDirection.S;
+                break;
+        }
+        return currentlyFacing;
+    }
+
+    private CompassDirection rotateRight(CompassDirection currentlyFacing){
+        switch (currentlyFacing) {
+            case N:
+                currentlyFacing = CompassDirection.E;
+                break;
+            case E:
+                currentlyFacing = CompassDirection.S;
+                break;
+            case S:
+                currentlyFacing = CompassDirection.W;
+                break;
+            case W:
+                currentlyFacing = CompassDirection.N;
+                break;
+        }
+        return currentlyFacing;
     }
 }
--- src/test/java/org/space/logic/RoverTest.java
+++ src/test/java/org/space/logic/RoverTest.java
@@ -12,27 +12,88 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoverTest {
 
     @Test
-    @DisplayName("start facing north and rotate left 4 times")
+    @DisplayName("start facing north and rotate left")
     void rotateLeft() {
         Position position = new Position(0,0, CompassDirection.N);
         Rover rover = new Rover(position);
 
         // Start now and rotate left
         Assertions.assertEquals(CompassDirection.W, rover.rotate(Instruction.L));
-        Assertions.assertEquals(CompassDirection.S, rover.rotate(Instruction.L));
-        Assertions.assertEquals(CompassDirection.E, rover.rotate(Instruction.L));
-        Assertions.assertEquals(CompassDirection.N, rover.rotate(Instruction.L));
     }
 
     @Test
-    @DisplayName("start facing north and rotate right 4 times")
-    void rotateRight() {
+    @DisplayName("start facing west and rotate left")
+    void rotateLeftFromWest() {
+        Position position = new Position(0,0, CompassDirection.W);
+        Rover rover = new Rover(position);
+
+        // Start now and rotate left
+        Assertions.assertEquals(CompassDirection.S, rover.rotate(Instruction.L));
+    }
+
+    @Test
+    @DisplayName("start facing south and rotate left")
+    void rotateLeftFromSouth() {
+        Position position = new Position(0,0, CompassDirection.S);
+        Rover rover = new Rover(position);
+
+        // Start now and rotate left
+        Assertions.assertEquals(CompassDirection.E, rover.rotate(Instruction.L));
+    }
+
+    @Test
+    @DisplayName("start facing east and rotate left")
+    void rotateLeftFromEast() {
+        Position position = new Position(0,0, CompassDirection.E);
+        Rover rover = new Rover(position);
+
+        // Start now and rotate left
+        Assertions.assertEquals(CompassDirection.N, rover.rotate(Instruction.L));
+    }
+
+
+    @Test
+    @DisplayName("start facing north and rotate right")
+    void rotateRightFromNorth() {
         Position position = new Position(0,0, CompassDirection.N);
         Rover rover = new Rover(position);
         // Start north and rotate right
         Assertions.assertEquals(CompassDirection.E, rover.rotate(Instruction.R));
+    }
+
+    @Test
+    @DisplayName("start facing east and rotate right")
+    void rotateRightFromEast() {
+        Position position = new Position(0,0, CompassDirection.E);
+        Rover rover = new Rover(position);
+        // Start north and rotate right
         Assertions.assertEquals(CompassDirection.S, rover.rotate(Instruction.R));
+    }
+
+    @Test
+    @DisplayName("start facing south and rotate right")
+    void rotateRightFromSouth() {
+        Position position = new Position(0,0, CompassDirection.S);
+        Rover rover = new Rover(position);
+        // Start north and rotate right
         Assertions.assertEquals(CompassDirection.W, rover.rotate(Instruction.R));
+    }
+
+    @Test
+    @DisplayName("start facing west and rotate right")
+    void rotateRightFromWest() {
+        Position position = new Position(0,0, CompassDirection.W);
+        Rover rover = new Rover(position);
+        // Start north and rotate right
         Assertions.assertEquals(CompassDirection.N, rover.rotate(Instruction.R));
+    }
+
+    @Test
+    @DisplayName("start facing west and Instruction.M should not change direction")
+    void rotateRightFromWestAndGetInstructionM() {
+        Position position = new Position(0,0, CompassDirection.W);
+        Rover rover = new Rover(position);
+        // Start north and rotate right
+        Assertions.assertEquals(CompassDirection.W, rover.rotate(Instruction.M));
     }
 }
