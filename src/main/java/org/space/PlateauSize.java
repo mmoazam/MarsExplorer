be4e3004cf81dc6df9d72bfee01f2d4399340df2
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-16 12:32:58 +0100

    add class PlateauSize

---
+++ src/main/java/org/space/PlateauSize.java
@@ -0,0 +1,21 @@
+package org.space;
+
+public class PlateauSize {
+    private int width;
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public PlateauSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+}
